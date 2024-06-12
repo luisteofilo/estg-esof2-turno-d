a9d7ac7415d8602ad5a71a641862c246fd1fb259
brunoferreira17 <brunoferreiraferreira17@gmail.com>
2024-06-12 16:30:04 +0100

    Implemented EmailTemplateController and EmailTemplateService
    - Added EmailTemplateController with endpoints for CRUD operations.
    - Implemented EmailTemplateService to handle database operations for EmailTemplate entities.
    - Updated ApplicationDbContext to include EmailTemplates DbSet.
    - Updated startup configuration to include mapping for EmailTemplateController.

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -8,11 +8,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-
 // Add services to the container.
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
