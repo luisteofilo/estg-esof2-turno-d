085857bdf53ae1a88e681104c4d83e9aa5975743
tiago <100705276+Tiago02p@users.noreply.github.com>
2024-06-12 11:11:51 +0100

    Metodos de Manipulação da Interview referentes ao State

---
+++ DBLayer/Extensions/InterviewExtensions.cs
@@ -0,0 +1,34 @@
+﻿using ESOF.WebApp.DBLayer.Entities;
+
+namespace ESOF.WebApp.DBLayer.Extensions;
+
+public static class InterviewExtensions
+{
+    public static void UpdateInterviewState(this Interview interview, InterviewState newState )
+    {
+        interview.InterviewState = newState;
+    }
+    
+    public static bool isOnGoing(this Interview interview)
+    {
+        if (interview.InterviewState == InterviewState.OnGoing)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static void setDateTimeOff(this Interview interview)
+    {
+        if (interview.isOnGoing())
+        {
+            /*Função para desativar o campo onde se insere o DateTime
+             A entrevista OnGoing (a decorrer) não precisa de ter data e hora
+             por isso quando a entrevista está a decorrer o campo de inserção
+             data e hora não pode estar disponivel*/
+        }
+    }
+}
