460c91702390c33418aef4b9dc916a8c22498fe1
juze23 <jose.edu.cruz.23.09@gmail.com>
2024-06-12 14:17:42 +0100

    entities and tables

---
+++ DBLayer/Context/Tables/Jobs.cs
@@ -0,0 +1,33 @@
+﻿using ESOF.WebApp.DBLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ESOF.WebApp.DBLayer.Context;
+
+public partial class Jobs
+{
+    private void BuildJobs(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Job>()
+            .Property(p => p.JobId)
+            .HasDefaultValueSql("gen_random_uuid()");
+        /*
+        modelBuilder.Entity<Job>()
+            .HasOne(j => j.Client)
+            .WithMany(c => c.Jobs)
+            .HasForeignKey(c => c.ClientId);
+        */
+        /*
+        modelBuilder.Entity<Job>()
+            .HasMany(j => j.RequiredSkills)
+            .WithOne(s => s.Jobs)           //Change to the many to many table
+            .HasForeignKey(s => s.JobId);
+        */
+        /*
+        modelBuilder.Entity<Job>()
+            .HasMany(j => j.NiceToHaveSkills)
+            .WithOne(s => s.Jobs)           //Change to the many to many table
+            .HasForeignKey(s => s.JobId);
+        */
+    }
+    
+}
