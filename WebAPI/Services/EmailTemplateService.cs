abae7d29d5e17d6b4906875dff73e3631c710b36
brunoferreira17 <brunoferreiraferreira17@gmail.com>
2024-06-12 16:30:04 +0100

    Implemented EmailTemplateController and EmailTemplateService
    - Added EmailTemplateController with endpoints for CRUD operations.
    - Implemented EmailTemplateService to handle database operations for EmailTemplate entities.
    - Updated ApplicationDbContext to include EmailTemplates DbSet.
    - Updated startup configuration to include mapping for EmailTemplateController.

--- WebAPI/Services/EmailTemplateService.cs
+++ WebAPI/Services/EmailTemplateService.cs
@@ -1,54 +1,55 @@
 ﻿using ESOF.WebApp.DBLayer.Context;
 using ESOF.WebApp.DBLayer.Entities.Emails;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace ESOF.WebApp.WebAPI.Services
 {
     public class EmailTemplateService
     {
-        private readonly ApplicationDbContext db;
-
-        public EmailTemplateService(ApplicationDbContext context)
-        {
-            db = context;
-        }
-
         // Método para obter todos os templates
         public async Task<List<EmailTemplate>> GetAllTemplatesAsync()
         {
-            return await db.EmailTemplates.ToListAsync();
+            var _db = new ApplicationDbContext();
+            
+            return await _db.EmailTemplates.ToListAsync();
         }
 
         // Método para obter um template específico pelo ID
-        public async Task<EmailTemplate> GetTemplateByIdAsync(int id)
+        public async Task<EmailTemplate?> GetTemplateByIdAsync(int id)
         {
-            return await db.EmailTemplates.FindAsync(id);
+            var _db = new ApplicationDbContext();
+            
+            return await _db.EmailTemplates.FindAsync(id);
         }
 
         // Método para adicionar um novo template
         public async Task AddTemplateAsync(EmailTemplate template)
         {
-            db.EmailTemplates.Add(template);
-            await db.SaveChangesAsync();
+            var _db = new ApplicationDbContext();
+            
+            _db.EmailTemplates.Add(template);
+            await _db.SaveChangesAsync();
         }
 
         // Método para atualizar um template existente
         public async Task UpdateTemplateAsync(EmailTemplate template)
         {
-            db.EmailTemplates.Update(template);
-            await db.SaveChangesAsync();
+            var _db = new ApplicationDbContext();
+            
+            _db.EmailTemplates.Update(template);
+            await _db.SaveChangesAsync();
         }
 
         // Método para deletar um template pelo ID
         public async Task DeleteTemplateAsync(int id)
         {
-            var template = await db.EmailTemplates.FindAsync(id);
+            var _db = new ApplicationDbContext();
+            var template = await _db.EmailTemplates.FindAsync(id);
+            
             if (template != null)
             {
-                db.EmailTemplates.Remove(template);
-                await db.SaveChangesAsync();
+                _db.EmailTemplates.Remove(template);
+                await _db.SaveChangesAsync();
             }
         }
     }
