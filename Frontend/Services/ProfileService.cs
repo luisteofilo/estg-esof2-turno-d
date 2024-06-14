698383f2c6260d16ad919487da2dc8c4acd4b7e5
NxT1me <goncalocsousa618@gmail.com>
2024-06-14 12:03:35 +0100

    Retorna todos os perfis

--- Frontend/Services/ProfileService.cs
+++ Frontend/Services/ProfileService.cs
@@ -121,4 +121,27 @@ public class ProfileService(HttpClient _httpClient) : IProfileService
         response.EnsureSuccessStatusCode();
     }
     
+    public async Task<ProfileDto> GetProfiles()
+    {
+        try
+        {
+            var response = await httpClient.GetAsync($"api/Profile");
+                
+            if (response.IsSuccessStatusCode)
+            {
+                if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
+                {
+                    return new ProfileDto();
+                }
+                return await response.Content.ReadFromJsonAsync<ProfileDto>();
+            }
+
+            var message = await response.Content.ReadAsStringAsync();
+            throw new Exception(message);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"An error occurred while fetching profile: {ex.Message}", ex);
+        }
+    }
 }
