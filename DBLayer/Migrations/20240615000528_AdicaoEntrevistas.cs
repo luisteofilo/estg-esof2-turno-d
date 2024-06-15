2dc81f5e155a16760b8e535cca7f9a0781ee4ed4
tiago <100705276+Tiago02p@users.noreply.github.com>
2024-06-15 01:08:52 +0100

    Migração

---
+++ DBLayer/Migrations/20240615000528_AdicaoEntrevistas.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ESOF.WebApp.DBLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicaoEntrevistas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Interviews",
+                columns: table => new
+                {
+                    InterviewId = table.Column<Guid>(type: "uuid", nullable: false),
+                    DateHour = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    InterviewState = table.Column<string>(type: "text", nullable: false),
+                    Location = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Interviews", x => x.InterviewId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Interviews");
+        }
+    }
+}
