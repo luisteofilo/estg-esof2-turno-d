a4f4d63e76bbf39280abe45c69832132cd4cf074
Bet20 <55533920+Bet20@users.noreply.github.com>
2024-06-19 10:27:57 +0100

    Minor fixes to styles and created the FAQ search window

--- WebAPI/Services/JobFAQService.cs
+++ WebAPI/Services/JobFAQService.cs
@@ -1,20 +1,34 @@
 using ESOF.WebApp.DBLayer.Context;
 using ESOF.WebApp.DBLayer.Entities;
 using ESOF.WebApp.DBLayer.Entities.FAQ;
+using Microsoft.EntityFrameworkCore;
 
 namespace ESOF.WebApp.WebAPI.Services;
 
 public class JobFAQService
 {
-    public static async Task AddQuestion(Guid jobId, string questionText)
+    private readonly ApplicationDbContext _db;
+    
+    public JobFAQService(ApplicationDbContext db)
     {
-        var db = new ApplicationDbContext();
+        _db = db;
+    }
+    public async Task AddQuestion(Guid jobId, string questionText)
+    {
         var question = new Question
         {
             QuestionText = questionText,
         };
-        db.FAQQuestions.Add(question);
-        await db.SaveChangesAsync();
+        _db.FAQQuestions.Add(question);
+        await _db.SaveChangesAsync();
+    }
+    
+    public async Task<List<Question>> SearchQuestions(string query)
+    {
+        var questions = _db.FAQQuestions.Where(q => 
+            q.QuestionText.Contains(query, StringComparison.CurrentCultureIgnoreCase) ||
+            q.Answers.Any(a => a.AnswerText.Contains(query, StringComparison.CurrentCultureIgnoreCase)));
+        return await questions.ToListAsync();
     }
     
 }
