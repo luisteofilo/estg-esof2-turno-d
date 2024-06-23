4f9db00550f6b0ca19720b1c4d135bb8af072736
tiago <100705276+Tiago02p@users.noreply.github.com>
2024-06-23 01:25:51 +0100

    DBLayer + Dto + Nova Migração Prontos para PR

---
+++ Common/Dtos/Interview/InterviewerDto.cs
@@ -0,0 +1,8 @@
+﻿using ESOF.WebApp.DBLayer.Entities;
+
+namespace Common.Dtos.Interview;
+public class InterviewerDto
+{
+    public Guid InterviewerId { get; set; }
+    public string Name { get; set; }
+}
---
+++ DBLayer/Context/Tables/Interviews/Interview.cs
@@ -0,0 +1,14 @@
+﻿using ESOF.WebApp.DBLayer.Entities.Interviews;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ESOF.WebApp.DBLayer.Context;
+
+public partial class ApplicationDbContext
+{
+    private void BuildInterviews(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Interview>()
+            .HasKey(e => new { e.InterviewId, e.InterviewerId,e.CandidateId});
+    }
+}
