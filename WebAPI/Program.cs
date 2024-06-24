fcf5d1cbce956560b2d4bcfc07a91f312f8229ab
alfredo <alfredoaguiar@ipvc.pt>
2024-06-24 17:07:28 +0100

    crected get

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddScoped<IProfileRepository, ProfileRepository>();
+builder.Services.AddScoped<ITaxonomias, TaxonomiasRep>();
 
 var app = builder.Build();
 
