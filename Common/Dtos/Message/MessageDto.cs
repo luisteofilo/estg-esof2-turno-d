0fa94cf2f37c74187a9ed7dc4b5a3ae023571958
João Freitas <jfreitasxd@gmail.com>
2024-06-26 02:21:29 +0100

    chat Design

---
+++ Common/Dtos/Message/MessageDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Common.Dtos.Message;
+
+public class MessageDto
+{
+    public string message { get; set; }
+    public string sender { get; set; }
+    public string data { get; set; }
+
+    public MessageDto(string sender, string message)
+    {
+        this.sender = sender;
+        this.message = message;
+        this.data = DateTime.Now.ToString("dd-MM-yyyy");
+    }
+}
