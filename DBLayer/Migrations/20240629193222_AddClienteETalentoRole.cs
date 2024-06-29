d7c011a4ce0d461a67512225be60f044ed1ac463
DiogoROliveira <diogo.rosas.oliveira@ipvc.pt>
2024-06-29 20:33:48 +0100

    Added migration

---
+++ DBLayer/Migrations/20240629193222_AddClienteETalentoRole.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ESOF.WebApp.DBLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddClienteETalentoRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var clienteRoleId = Guid.NewGuid();
+            var talentoRoleId = Guid.NewGuid();
+
+            // Inserir novas roles na tabela Roles
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "RoleId", "Name" },
+                values: new object[,]
+                {
+                    { clienteRoleId, "Cliente" },
+                    { talentoRoleId, "Talento" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Roles WHERE Name IN ('Cliente', 'Talento')");
+        }
+    }
+}
