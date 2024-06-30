6a2097d8c413ce42c5eabe732969b7c1aa96a5e9
Paulo Amorim <paulo.amorim@ipvc.pt>
2024-06-30 18:00:03 +0100

    corrigir conflitos addinvoice

--- DBLayer/Migrations/20240626154422_AddInvoiceToDatabase.cs
+++ DBLayer/Migrations/20240626154422_AddInvoiceToDatabase.cs
@@ -26,133 +26,15 @@ namespace ESOF.WebApp.DBLayer.Migrations
                     table.PrimaryKey("PK_Invoices", x => x.InvoiceId);
                 });
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Educations_ProfileId",
-                table: "Educations",
-                column: "ProfileId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Experiences_ProfileId",
-                table: "Experiences",
-                column: "ProfileId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Interviews_CandidateId",
-                table: "Interviews",
-                column: "CandidateId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Interviews_InterviewerId",
-                table: "Interviews",
-                column: "InterviewerId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Jobs_ImportId",
-                table: "Jobs",
-                column: "ImportId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_JobSkills_SkillId",
-                table: "JobSkills",
-                column: "SkillId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Positions_JobId",
-                table: "Positions",
-                column: "JobId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Profiles_UserId",
-                table: "Profiles",
-                column: "UserId",
-                unique: true);
-
-            migrationBuilder.CreateIndex(
-                name: "IX_ProfileSkills_SkillId",
-                table: "ProfileSkills",
-                column: "SkillId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_RolePermissions_PermissionId",
-                table: "RolePermissions",
-                column: "PermissionId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Timesheets_PositionId",
-                table: "Timesheets",
-                column: "PositionId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_UserRoles_UserId",
-                table: "UserRoles",
-                column: "UserId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Users_Email",
-                table: "Users",
-                column: "Email",
-                unique: true);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "Educations");
-
-            migrationBuilder.DropTable(
-                name: "Experiences");
-
-            migrationBuilder.DropTable(
-                name: "Interviews");
-
-            migrationBuilder.DropTable(
-                name: "JobSkills");
-
-            migrationBuilder.DropTable(
-                name: "ProfileSkills");
-
-            migrationBuilder.DropTable(
-                name: "RolePermissions");
-
-            migrationBuilder.DropTable(
-                name: "Timesheets");
-
-            migrationBuilder.DropTable(
-                name: "UserRoles");
-
-            migrationBuilder.DropTable(
-                name: "Candidates");
-
-            migrationBuilder.DropTable(
-                name: "Interviewers");
-
-            migrationBuilder.DropTable(
-                name: "Profiles");
-
-            migrationBuilder.DropTable(
-                name: "Skills");
-
-            migrationBuilder.DropTable(
-                name: "Permissions");
 
             migrationBuilder.DropTable(
                 name: "Invoices");
 
-            migrationBuilder.DropTable(
-                name: "Positions");
-
-            migrationBuilder.DropTable(
-                name: "Roles");
-
-            migrationBuilder.DropTable(
-                name: "Users");
-
-            migrationBuilder.DropTable(
-                name: "Jobs");
-
-            migrationBuilder.DropTable(
-                name: "Imports");
         }
     }
 }
