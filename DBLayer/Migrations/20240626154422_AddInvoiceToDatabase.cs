c741499ee372f269d36e8e7b64266618eb9cfee9
Paulo Amorim <paulo.amorim@ipvc.pt>
2024-06-30 18:18:04 +0100

    correção migração Invoices

--- DBLayer/Migrations/20240626154422_AddInvoiceToDatabase.cs
+++ DBLayer/Migrations/20240626154422_AddInvoiceToDatabase.cs
@@ -29,6 +29,39 @@ namespace ESOF.WebApp.DBLayer.Migrations
                         principalColumn: "TimesheetId",
                         onDelete: ReferentialAction.Cascade);
                 });
+            
+            migrationBuilder.CreateTable(
+                name: "Timesheets",
+                columns: table => new
+                {
+                    TimesheetId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    HoursWorked = table.Column<int>(type: "integer", nullable: false),
+                    TaskDescription = table.Column<string>(type: "text", nullable: false),
+                    PositionId = table.Column<Guid>(type: "uuid", nullable: false),
+                    InvoiceId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Timesheets", x => x.TimesheetId);
+                    table.ForeignKey(
+                        name: "FK_Timesheets_Invoices_TimesheetId",
+                        column: x => x.TimesheetId,
+                        principalTable: "Invoices",
+                        principalColumn: "InvoiceId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Timesheets_Positions_PositionId",
+                        column: x => x.PositionId,
+                        principalTable: "Positions",
+                        principalColumn: "PositionId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+            
+            migrationBuilder.CreateIndex(
+                name: "IX_Timesheets_PositionId",
+                table: "Timesheets",
+                column: "PositionId");
 
             migrationBuilder.CreateIndex(
                 name: "IX_Invoices_TimesheetId",
@@ -40,6 +73,9 @@ namespace ESOF.WebApp.DBLayer.Migrations
         {
             migrationBuilder.Sql("DO $$ BEGIN IF EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'FK_Invoices_Timesheets_TimesheetId') THEN ALTER TABLE \"Invoices\" DROP CONSTRAINT \"FK_Invoices_Timesheets_TimesheetId\"; END IF; END $$;");
 
+            migrationBuilder.DropTable(
+                name: "Timesheets");
+            
             migrationBuilder.DropTable(
                 name: "Invoices");
             
